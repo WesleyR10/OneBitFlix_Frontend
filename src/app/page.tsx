adb04f50644b7291ec2ad1017f3b325ffcb082a0
WesleyR10 <wesleyribas2015@gmail.com>
2023-10-20 00:56:51 -0300

    Config metadata - favicon - title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 import Head from "next/head";
 import styles from "../../styles/HomeNoAuth.module.scss"
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Onebitflix',
+  description: 'Tenha acesso aos melhores conteúdos sobre programação de uma forma simples e fácil.',
+}
 
 const HomeNotAuth = function () {
   return (
     <>
       <Head>
-        <title>Home</title>
       </Head>
       <main></main>
     </>
