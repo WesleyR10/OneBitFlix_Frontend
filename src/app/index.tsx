b01e81cd1aa7d08348f3eeee882ef7d1cb4346c5
WesleyR10 <wesleyribas2015@gmail.com>
2023-10-20 00:23:50 -0300

    Project configuration, folders and installation

---
+++ src/app/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import styles from "../../styles/HomeNoAuth.module.scss"
+
+const HomeNotAuth = function () {
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <main></main>
+    </>
+  )
+};
+
+export default HomeNotAuth;
