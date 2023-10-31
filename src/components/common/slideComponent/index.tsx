ef7ba7ab2ed9be59302b3f0bd77e1c75f25d2c7c
WesleyR10 <wesleyribas2015@gmail.com>
2023-10-31 11:57:41 -0300

    Slide session responsiveness

--- src/components/common/slideComponent/index.tsx
+++ src/components/common/slideComponent/index.tsx
@@ -8,16 +8,49 @@ interface props {
 }
 
 const SlideComponent = ({ course }: props) => {
+  let slideCount = 0;
+
+  if (course.length > 4) {
+    slideCount = 4
+  } else if (course) {
+    slideCount = course.length
+  }
   return (
     <>
       <div className="d-flex flex-column align-items-center py-4">
         <Splide
           options={{
             type: "loop",
-            perPage: 4,
+            perPage: slideCount,
             perMove: 1,
-            width: 1200,
+            width: slideCount * 300,
             pagination: false,
+            arrows: course.length > 4 ? true : false,
+            drag: course.length > 4 ? true : false,
+            breakpoints: {
+              1200: {
+                perPage: slideCount >= 3 ? 3 : 2,
+                width: slideCount >= 3 ? 990 : 600,
+                arrows: course.length > 3 ? true : false,
+                drag: course.length > 3 ? true : false,
+
+              },
+              990: {
+                perPage: slideCount >= 2 ? 2 : 1,
+                width: slideCount >= 2 ? 600 : 300,
+                arrows: course.length > 2 ? true : false,
+                drag: course.length > 2 ? true : false,
+              },
+              600: {
+                perPage: 1,
+                width: 300,
+                arrows: course.length > 1 ? true : false,
+                drag: course.length > 1 ? true : false,
+              },
+              300: {
+                width: 250,
+              },
+            },
           }}
         >
           {course?.map((course) => (
