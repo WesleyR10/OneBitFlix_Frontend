79b7b421bed7d4c0752658695d5f72c555935f4d
WesleyR10 <wesleyribas2015@gmail.com>
2023-11-08 00:50:43 -0300

    create form element  password -styles

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,12 +1,16 @@
 import Head from "next/head";
+import { useState } from "react"
 import styles from "../styles/profile.module.scss"
 import HeaderAuth from "@/components/common/headerAuth";
 import UserForm from "@/components/profile/user";
 import { Row, Col, Container, Button } from "reactstrap";
 import Footer from "@/components/common/footer";
+import PasswordForm from "@/components/profile/password";
 
 
 const UserInfo = function () {
+  const [form, setForm] = useState("userForm")
+
   return (
     <>
       <Head>
@@ -19,11 +23,11 @@ const UserInfo = function () {
           <p className={styles.title}>Minha Conta</p>
           <Row className="pt-3 pb-5">
             <Col md={4} className={styles.btnColumn}>
-              <Button className={styles.renderForm}> DADOS PESSOAIS</Button>
-              <Button className={styles.renderForm}> SENHA</Button>
+              <Button className={styles.renderForm} onClick={() => { setForm("userForm") }} style={{ color: form === "userForm" ? "#FF0044" : "white" }}> DADOS PESSOAIS</Button>
+              <Button className={styles.renderForm} onClick={() => { setForm("passwordForm") }} style={{ color: form === "passwordForm" ? "#FF0044" : "white" }}> SENHA</Button>
             </Col>
             <Col md>
-              <UserForm />
+              {form === "userForm" ? <UserForm /> : <PasswordForm />}
             </Col>
           </Row>
         </Container>
--- src/components/profile/password/index.tsx
+++ src/components/profile/password/index.tsx
@@ -1,1 +1,33 @@
+import { Button, Form, FormGroup, Input, Label } from "reactstrap"
 import styles from "../../../../styles/profile.module.scss"
+
+const PasswordForm = function () {
+  return (
+    <>
+      <Form className={styles.form}>
+        <div className={styles.inputNormalDiv}>
+          <FormGroup>
+            <Label for="currentPassword" className={styles.label}>SENHA ATUAL</Label>
+            <Input name="currentPassword" type="password" id="currentPassword" placeholder="********" required minLength={6} maxLength={12} className={styles.input} />
+          </FormGroup>
+        </div>
+        <div className={styles.inputFlexDiv}>
+          <FormGroup>
+            <Label for="newPassword" className={styles.label}>NOVA SENHA</Label>
+            <Input name="newPassword" type="password" id="newPassword" placeholder="********" required minLength={6} maxLength={12} className={styles.inputFlex} />
+          </FormGroup>
+          <FormGroup>
+            <Label for="confirmNewPassword" className={styles.label}>CONFIRMAR NOVA SENHA</Label>
+            <Input name="confirmNewPassword" type="password" id="confirmNewPassword" placeholder="********" required minLength={6} maxLength={12} className={styles.inputFlex} />
+          </FormGroup>
+        </div>
+
+        <Button className={styles.formBtn} outline>
+          Salvar Alterações
+        </Button>
+      </Form>
+    </>
+  )
+}
+
+export default PasswordForm
