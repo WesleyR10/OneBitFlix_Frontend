5df60ea6f706f02eeaa6fa041d8e013cb714fd2b
WesleyR10 <wesleyribas2015@gmail.com>
2023-11-09 00:40:42 -0300

    Visual search creation, element and responsiveness

--- pages/search.tsx
+++ pages/search.tsx
@@ -4,7 +4,10 @@ import Head from "next/head";
 import HeaderAuth from "../src/components/common/headerAuth";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import { Container } from "reactstrap";
 import courseService, { CourseType } from "@/services/courseService";
+import SearchCard from "@/components/searchCard";
+import Footer from "@/components/common/footer";
 
 const Search = function () {
   const router = useRouter();
@@ -26,13 +29,24 @@ const Search = function () {
         <title>Onebitflix - {searchName}</title>
         <link rel="shortcut icon" href="/icon.svg" type="image/x-icon" />
       </Head>
-      <main>
-        <HeaderAuth />
-        {searchResult?.map((course) => (
-          <div key={course.id}>
-            <p>{course.name}</p>
+      <main className={styles.main}>
+        <div className={styles.headFooterBg}>
+          <HeaderAuth />
+        </div>
+        {searchResult.length >= 1 ? (
+          <div className={styles.searchResult}>
+            <Container className="d-flex flex-wrap justify-content-center gap-5 py-4">
+              {searchResult?.map((course) => (
+                <SearchCard key={course.id} course={course} />
+              ))}
+            </Container>
           </div>
-        ))}
+        ) : (
+          <p className={styles.noSearchText}>Nenhum resultado encontrado!</p>
+        )}
+        <div className={styles.headFooterBg}>
+          <Footer />
+        </div>
       </main>
     </>
   );
---
+++ src/components/searchCard/index.tsx
@@ -0,0 +1,22 @@
+/* eslint-disable @next/next/no-img-element */
+import Link from "next/link";
+import styles from "./styles.module.scss";
+import { CourseType } from "@/services/courseService";
+
+
+interface props {
+  course: CourseType;
+}
+const SearchCard = function ({ course }: props) {
+  return <>
+    <Link href={`/courses/${course.id}`} className="link">
+      <div className={styles.searchCard}>
+        <img src={`${process.env.NEXT_PUBLIC_BASEURL}/${course.thumbnailUrl}`} alt={course.name} className={styles.searchCardImg} />
+        <p className={styles.searchCardTitle}>{course.name}</p>
+        <p className={styles.searchCardDescription}>{course.synopsis}</p>
+      </div>
+    </Link>
+  </>;
+};
+
+export default SearchCard;
